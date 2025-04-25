92c253e2b19a26e663bf5b8251094089ce30e1ac
johannes <johannestoke@gmail.com>
2025-04-26 00:46:31 +0200

    initial commit

---
+++ join_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cat parts/dtu_34871-NL-Transducers.tar.zst.parta* > tars/dtu_34871-NL-Transducers.tar.zst
+tar xvf tars/dtu_34871-NL-Transducers.tar.zst
+cat parts/speakers.tar.zst.parta* > tars/speakers.tar.zst
+tar xvf tars/speakers.tar.zst
